12349100e468aeddedc93f96427ebf826c4f90a4
JacobAdamson <jadamson@colby.edu>
2017-02-22 21:54:49 -0500

    added stmt

--- test/bantam/parser/ParserTest.java
+++ test/bantam/parser/ParserTest.java
@@ -43,6 +43,40 @@ public class ParserTest
         assertEquals(0, mainClass.getMemberList().getSize());
     }
 
+    /**
+     * tests the case of a missing right brace at end of a class def
+     * using an ExpectedException Rule
+     */
+    @Test
+    public void unmatchedLeftBraceTest1() throws Exception {
+        Parser parser = new Parser(new Lexer(new StringReader("class Main {  ")));
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage("Bantam parser found errors.");
+        parser.parse();
+    }
+
+    /**
+     * tests the case of a missing right brace at end of a class def.
+     * This version is like unmatchedLeftBraceTest1 except that it
+     * doesn't use an ExpectedException Rule and it also prints the error messages.
+     */
+    @Test
+    public void unmatchedLeftBraceTest2() throws Exception {
+        Parser parser = new Parser(new Lexer(new StringReader("class Main {  ")));
+        boolean thrown = false;
+
+        try {
+            parser.parse();
+        } catch (RuntimeException e) {
+            thrown = true;
+            assertEquals("Bantam parser found errors.", e.getMessage());
+            for (ErrorHandler.Error err : parser.getErrorHandler().getErrorList()) {
+                System.out.println(err);
+            }
+        }
+        assertTrue(thrown);
+    }
+
     @Test
     /**
      * Tests the case of a method
@@ -117,6 +151,37 @@ public class ParserTest
         assertTrue(method.getStmtList().get(1) instanceof UnaryIncrExpr);
     }
 
+    /**
+     * Tests the UnaryIncrExpr for Post Unary Ops
+     * @throws Exception
+     */
+    @Test
+    public void unaryNotExpr() throws Exception{
+        Method method = getMethod("class Main{void test(){boolean test != true;}}");
+        assertTrue(method.getStmtList().get(1) instanceof UnaryIncrExpr);
+    }
+
+    /**
+     * Tests the UnaryIncrExpr for Post Unary Ops
+     * @throws Exception
+     */
+    @Test
+    public void unaryNegExpr() throws Exception{
+        Method method = getMethod("class Main{void test(){int test = -1; }}");
+        assertTrue(method.getStmtList().get(1) instanceof UnaryIncrExpr);
+    }
+
+    /**
+     * Gets the first statement
+     * @param s the java program string
+     * @param index
+     * @return
+     */
+    @Test
+    public Stmt getStmt(String s, int index)throws Exception{
+        return (Stmt)getMethod(s).getStmtList().get(index);
+    }
+
     public Method getMethod(String s)throws Exception{
         Class_ myClass = getClass(s);
         return (Method)myClass.getMemberList().get(0);
@@ -127,40 +192,5 @@ public class ParserTest
         Symbol result = parser.parse();
         ClassList classes = ((Program) result.value).getClassList();
         return (Class_) classes.get(0);
-    }
-
-
-    /**
-     * tests the case of a missing right brace at end of a class def
-     * using an ExpectedException Rule
-     */
-    @Test
-    public void unmatchedLeftBraceTest1() throws Exception {
-        Parser parser = new Parser(new Lexer(new StringReader("class Main {  ")));
-        thrown.expect(RuntimeException.class);
-        thrown.expectMessage("Bantam parser found errors.");
-        parser.parse();
-    }
-
-    /**
-     * tests the case of a missing right brace at end of a class def.
-     * This version is like unmatchedLeftBraceTest1 except that it
-     * doesn't use an ExpectedException Rule and it also prints the error messages.
-     */
-    @Test
-    public void unmatchedLeftBraceTest2() throws Exception {
-        Parser parser = new Parser(new Lexer(new StringReader("class Main {  ")));
-        boolean thrown = false;
-
-        try {
-            parser.parse();
-        } catch (RuntimeException e) {
-            thrown = true;
-            assertEquals("Bantam parser found errors.", e.getMessage());
-            for (ErrorHandler.Error err : parser.getErrorHandler().getErrorList()) {
-                System.out.println(err);
-            }
-        }
-        assertTrue(thrown);
     }
 }
