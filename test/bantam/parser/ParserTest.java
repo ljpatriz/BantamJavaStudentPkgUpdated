2171a27a6b6540b804a91afbec5a6c97b666d626
JacobAdamson <jadamson@colby.edu>
2017-02-22 21:44:20 -0500

    refactored the junk junk junk

--- test/bantam/parser/ParserTest.java
+++ test/bantam/parser/ParserTest.java
@@ -48,23 +48,17 @@ public class ParserTest
      * Tests the case of a method
      */
     public void emptyMethodTest() throws Exception{
-        Parser parser = new Parser(new Lexer(new StringReader("class Main{ int myInt(){ }}")));
-        Symbol result = parser.parse();
-        ClassList classes = ((Program) result.value).getClassList();
-        Class_ mainClass = (Class_) classes.get(0);
-        Method method = (Method)mainClass.getMemberList().get(0);
+        Method method = getMethod("class Main{ int myInt(){ }}");
         assertEquals("myInt", method.getName());
     }
 
-    @Test
+
     /**
      * Tests the case of a field
      */
+    @Test
     public void fieldTest() throws Exception{
-        Parser parser = new Parser(new Lexer(new StringReader("class Main{int a;}")));
-        Symbol result = parser.parse();
-        ClassList classes = ((Program) result.value).getClassList();
-        Class_ mainClass = (Class_) classes.get(0);
+        Class_ mainClass = getClass("class Main{int a;}");
         Field field = (Field)mainClass.getMemberList().get(0);
         assertEquals("a", field.getName());
         assertEquals("int", field.getType());
@@ -75,10 +69,7 @@ public class ParserTest
      */
     @Test
     public void initializedFieldTest() throws Exception{
-        Parser parser = new Parser(new Lexer(new StringReader("class Main{int a = 0;}")));
-        Symbol result = parser.parse();
-        ClassList classes = ((Program) result.value).getClassList();
-        Class_ mainClass = (Class_) classes.get(0);
+        Class_ mainClass = getClass("class Main{int a = 0;}");
         Field field = (Field)mainClass.getMemberList().get(0);
         assertEquals("a", field.getName());
         assertEquals("int", field.getType());
@@ -92,12 +83,32 @@ public class ParserTest
      */
     @Test
     public void unaryDecrExprTest() throws Exception{
-        Parser parser = new Parser(new Lexer(new StringReader("class Main{void test(){int test = 1; test--;}}")));
+        Method method = getMethod("class Main{void test(){int test = 1; test--;}}");
+        assertTrue(method.getStmtList().get(1) instanceof UnaryDecrExpr);
+    }
+
+    /**
+     * Tests the UnaryIncrExpr
+     * @throws Exception
+     */
+    @Test
+    public void unaryIncrExprTest() throws Exception{
+        Method method = getMethod("class Main{void test(){int test = 1; test++;}}");
+        assertTrue(method.getStmtList().get(1) instanceof UnaryIncrExpr);
+    }
+
+    public Method getMethod(String s)throws Exception{
+        Class_ myClass = getClass(s);
+        return (Method)myClass.getMemberList().get(0);;
+    }
+
+    public Class_ getClass(String s) throws Exception{
+        Parser parser = new Parser(new Lexer(new StringReader(s)));
         Symbol result = parser.parse();
         ClassList classes = ((Program) result.value).getClassList();
-        Class_ mainClass = (Class_) classes.get(0);
-        Method method = (Method)mainClass.getMemberList().get(0);
+        return (Class_) classes.get(0);
     }
+
 
     /**
      * tests the case of a missing right brace at end of a class def
