9090dfb0d25b7e56fd64df05823e39a206e44252
ljpatriz <ljpatriz@gmail.com>
2017-02-27 21:51:34 -0500

    small fix

--- src/bantam/visitor/StringConstantsVisitor.java
+++ src/bantam/visitor/StringConstantsVisitor.java
@@ -42,7 +42,7 @@ public class StringConstantsVisitor extends Visitor{
      * @return
      */
     public Object visit(ConstStringExpr constStringExpr){
-        this.nameBuilder.setLength(11);
+        this.nameBuilder.setLength(12);
         this.nameBuilder.append(stringConstantMap.entrySet().size());
 
         stringConstantMap.put(this.nameBuilder.toString(),
