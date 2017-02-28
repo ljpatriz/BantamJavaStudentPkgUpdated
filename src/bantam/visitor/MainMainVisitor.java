8fba6d490eaf239046097f0f0a32782e0ec2f49f
nickcameron <ncameron@colby.edu>
2017-02-27 19:13:52 -0500

    Add visit methods to MainMainVisitor class to prevent unnecessary AST traversal

--- src/bantam/visitor/MainMainVisitor.java
+++ src/bantam/visitor/MainMainVisitor.java
@@ -1,6 +1,8 @@
 package bantam.visitor;
 
 import bantam.ast.Class_;
+import bantam.ast.Field;
+import bantam.ast.Method;
 import bantam.ast.Program;
 
 /**
@@ -8,7 +10,7 @@ import bantam.ast.Program;
  */
 public class MainMainVisitor extends Visitor {
 
-    private String currentClass = "";
+    private boolean has;
 
     /**
      * Returns true if the given program contains a Main class with a main method
@@ -18,22 +20,53 @@ public class MainMainVisitor extends Visitor {
      * @return whether has a main class or not
      */
     public boolean hasMain(Program ast) {
+        this.has = false;
         this.visit(ast);
-        return this.currentClass.equals("Main");
+        return this.has;
     }
 
     /**
-     * Visits a Class_ node and updates the currentClass string if the
-     * currentClass is not "Main"
+     * Visits a Class_ node and searches for a main method if the class name is Main
      * @param node the class node
      * @return an Object
      */
     @Override
     public Object visit(Class_ node) {
-        super.visit(node);
-        if (!this.currentClass.equals("Main"))
-            this.currentClass = node.getName();
+        if (node.getName().equals("Main")) {
+            super.visit(node);
+        }
 
+        return null;
+    }
+
+    /**
+     * Visits a method node in search of a
+     * void main() method with no parameters.
+     * Updates the has field if such a method is found.
+     * @param node the method node
+     * @return
+     */
+    @Override
+    public Object visit(Method node) {
+        if (!node.getName().equals("main"))
+            return null;
+        if (!node.getReturnType().equals("void"))
+            return null;
+        if (node.getFormalList().getSize() != 0)
+            return null;
+
+        this.has = true;
+        // do not descend further for efficient traversal
+        return null;
+    }
+
+    /**
+     * Prevents descending any further than a Field from a Member in the grammar
+     * @param node the field node
+     * @return
+     */
+    @Override
+    public Object visit(Field node) {
         return null;
     }
 }
