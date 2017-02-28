fb0d0a3c6c36efd78d27807015fe084a7788ecf7
JacobAdamson <jadamson@colby.edu>
2017-02-27 21:31:06 -0500

    javadoc'd string constants vistor

--- src/bantam/visitor/StringConstantsVisitor.java
+++ src/bantam/visitor/StringConstantsVisitor.java
@@ -7,20 +7,40 @@ import bantam.ast.Program;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by Jacob on 27/02/17.
+/*
+ * File: ParserTest.java
+ * Author: Jacob, Nick, Larry, Luis, CP
+ * Date: 2/23/17
  */
 public class StringConstantsVisitor extends Visitor{
 
+    /**
+     * This map contains all the Strings and their assigned names
+     */
     private Map<String, String> stringConstantMap;
+
+    /**
+     * This Builds the String Constants for the names
+     */
     private StringBuilder nameBuilder = new StringBuilder("StringConst_");
 
+    /**
+     * Returns a map containing Map whose keys are the String constants from the program
+     * and whose values are names for the String constants.
+     * @param ast Abstract Syntax Tree
+     * @return
+     */
     public Map<String, String> getStringConstants(Program ast){
         stringConstantMap = new HashMap<>();
         this.visit(ast);
         return stringConstantMap;
     }
 
+    /**
+     * Override method for the string constant. Adds this string to the map.
+     * @param constStringExpr
+     * @return
+     */
     public Object visit(ConstStringExpr constStringExpr){
         this.nameBuilder.setLength(11);
         this.nameBuilder.append(stringConstantMap.entrySet().size());
@@ -31,6 +51,12 @@ public class StringConstantsVisitor extends Visitor{
         return null;
     }
 
+    /**
+     * Optimization function as formals cannot be string Constants
+     * Does nothing
+     * @param formal
+     * @return
+     */
     public Object visit(Formal formal){
         return null;
     }
