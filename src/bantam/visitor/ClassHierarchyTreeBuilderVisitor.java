8327d6ef57c90b2606f4d561dc319f9bc3f0a7d6
JacobAdamson <jadamson@colby.edu>
2017-03-01 11:39:31 -0500

    started ClassHeiarchyTreeBuilderVisitor class for step one of project

---
+++ src/bantam/visitor/ClassHierarchyTreeBuilderVisitor.java
@@ -0,0 +1,81 @@
+package bantam.visitor;
+
+import bantam.ast.Class_;
+import bantam.ast.Program;
+import bantam.util.ClassTreeNode;
+
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ * Created by Jacob on 01/03/17.
+ */
+public class ClassHierarchyTreeBuilderVisitor extends Visitor {
+
+    private ClassTreeNode classTreeRootNode;
+    private Hashtable<String, ClassTreeNode> classMap;
+
+    public ClassHierarchyTreeBuilderVisitor(){
+        classMap = new Hashtable<>();
+    }
+
+    public ClassTreeNode buildClassTree(Program program){
+        classTreeRootNode = buildBuiltinTree();
+        this.visit(program);
+        buildInheritanceTree();
+        if(hasCycles()){
+            //TODO create a specific error class for this
+            throw new RuntimeException("Inheritance Tree has Cycles");
+        }
+        return classTreeRootNode;
+    }
+
+    private boolean hasCycles{
+
+    }
+
+
+    /**
+     * Function for building all the tree of default classes
+     * @return
+     */
+    private ClassTreeNode buildBuiltinTree(){
+        classTreeRootNode = new ClassTreeNode();
+        //TODO find out how to do this
+        return classTreeRootNode;
+    }
+
+    /**
+     * Function for visiting the class Node
+     * @param classNode
+     * @return
+     */
+    public Object visit(Class_ classNode){
+        ClassTreeNode classTreeNode = new ClassTreeNode(classNode,true,false,classMap);
+
+        if(classMap.containsKey(classNode.getName())){
+            throw new RuntimeException("Class Tree has duplicate class names");
+        }
+        
+        else{
+            classMap.put(classNode.getName(),classTreeNode);
+        }
+
+        //TODO add stuff for adding, making method tables
+        return null;
+    }
+
+    /**
+     * Builds the inheritance tree using the classMap that was already generated
+     */
+    private void buildInheritanceTree(){
+        for(Entry<String, ClassTreeNode> entry:classMap.entrySet()){
+            ClassTreeNode classTreeNode = entry.getValue();
+            String parent = classTreeNode.getASTNode().getParent();
+            ClassTreeNode parentTreeNode = classMap.get(parent)
+            parentTreeNode.addChild(classTreeNode);
+            classTreeNode.setParent(parentTreeNode);
+        }
+    }
+}
