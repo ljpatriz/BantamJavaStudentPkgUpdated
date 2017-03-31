c22aee9523be645fc9a78be602a988ef59843e0d
nickcameron <ncameron@colby.edu>
2017-03-31 03:42:18 -0400

    Done parts 1-6 hopefully. Several TODOs left to address.

--- src/bantam/codegenmips/MipsCodeGenerator.java
+++ src/bantam/codegenmips/MipsCodeGenerator.java
@@ -26,6 +26,14 @@
 
 package bantam.codegenmips;
 
+//// TODO: 3/30/2017 dale wants any new classes or changed classes to go in this package
+//// TODO: 3/31/2017 figure out how to test this
+//// TODO: 3/31/2017 test this
+//// TODO: 3/31/2017 other to-dos
+
+
+import bantam.ast.Class_;
+import bantam.ast.Method;
 import bantam.ast.Program;
 import bantam.util.ClassTreeNode;
 import bantam.util.SymbolTable;
@@ -34,8 +42,7 @@ import bantam.visitor.StringConstantsVisitor;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 /**
  * The <tt>MipsCodeGenerator</tt> class generates mips assembly code
@@ -117,27 +124,39 @@ public class MipsCodeGenerator {
      * See the lab manual for the details of each of these steps.
      */
     public void generate() {
-        //// TODO: 3/30/2017 generate comment header - how do we get the author & date?
+        assemblySupport.genComment(" Author:\tUnknown");    //// TODO: 3/31/2017
+        assemblySupport.genComment(" Date:\tMarch, 2017");
+        assemblySupport.genComment(" Compiled from sources:");
+        assemblySupport.genComment(" \t" + " Unknown");     //// TODO: 3/31/2017
+
         //1 - start the data section
         assemblySupport.genDataStart();
 
         //2 - generate data for the garbage collector
         assemblySupport.genLabel("gc_flag");
-        assemblySupport.genWord("0");
+        assemblySupport.genWord(this.gc ? "1": "0");
 
-        //3 - generate string constants
+        //3 - generate string constants, including class names
         generateStringConstants();
 
         //4 - generate class name table
         generateClassNameTable();
 
+        //5 - generate object templates
         generateTemplates();
+
         //6 - generate dispatch tables
+        generateDispatchTables();
+
         //7 - start the text section
         //8 - generate initialization subroutines
         //9 - generate user-defined methods
     }
 
+    /**
+     * This method generates the assembly code for all string constants, including both
+     * string constants found in the code, and string constants for class names.
+     */
     public void generateStringConstants(){
         StringConstantsVisitor stringConstantsVisitor = new StringConstantsVisitor();
 
@@ -150,22 +169,46 @@ public class MipsCodeGenerator {
         }
 
         //generate class name strings
+        //// TODO: 3/30/2017 do we want it to be in the right order ?
         int classNum = 0;
         for(ClassTreeNode node: this.root.getClassMap().values()){
-            generateStringConstant(node.getName(), "class_name_"+classNum);
+            generateStringConstant("class_name_"+classNum, node.getName());
             classNum++;
         }
     }
 
     /**
-     * This methods generates the class name strings and the class name table
-     * It puts into the table all the class name string labels
-     *  as well as the template labels
+     * This method creates the assembly for a string constant.
+     * @param label the label for the string
+     * @param string the string itself
+     */
+    public void generateStringConstant(String label, String string){
+        int stringLengthRounded = (int) Math.ceil((string.length()+1)/4.0)*4; //remember
+        // to look this line over with dale,
+        //add one or not to add one, that is the question
+        assemblySupport.genLabel(label);
+        assemblySupport.genWord("1"); //says its a string?
+
+        assemblySupport.genWord(Integer.toString(16 + stringLengthRounded)); //size of
+        // all of this
+
+        assemblySupport.genWord("String_dispatch_table"); //pointer to VFT
+        assemblySupport.genWord(Integer.toString(string.length()));
+        //// TODO: 3/31/2017 this line is in example, but we should ask dale. ^^^^
+        assemblySupport.genAscii(string); //the actual string
+        assemblySupport.genByte("0"); // null terminator
+        assemblySupport.genAlign(); // align the bytes properly
+    }
+
+    /**
+     * This methods generates the full class name table, containing the labels for the
+     * class_name strings and the global declarations for the class templates.
      */
     public void generateClassNameTable(){
         assemblySupport.genLabel("class_name_table");
         //generate class name words
         int numClasses = this.root.getClassMap().values().size();
+        // note that order doesn't matter in the below iteration
         for(int i = 0; i < numClasses; i++){
             assemblySupport.genWord("class_name_"+i);
         }
@@ -176,74 +219,107 @@ public class MipsCodeGenerator {
     }
 
     /**
-     * Generates the templates for all the classes
+     * Generates the templates for all the classes.
      */
     public void generateTemplates(){
         //TODO be more elegant than this
-        int classid = 0;
+        int classId = 0;
         for(ClassTreeNode node : this.root.getClassMap().values()){
-            generateClassTemplate(node, classid);
-            classid++;
+            generateClassTemplate(node, classId);
+            classId++;
         }
+
+//        //// TODO: 3/31/2017 this is the only reasonable alternative Jacob -- Nick
+//        //   I think it looks much worse
+//
+//        ListIterator<ClassTreeNode> it = new ArrayList<>(
+//                this.root.getClassMap().values()).listIterator();
+//        while (it.hasNext()) {
+//            int classId = it.nextIndex();
+//            generateClassTemplate(it.next(), classId);
+//        }
     }
 
     /**
-     * Generates the template for the given class
-     * @param node
+     * Generates the template for the given class with the given id.
+     * @param node the ClassTreeNode corresponding to the class
+     * @param id the int id of the class that is having a template generated
      */
     public void generateClassTemplate(ClassTreeNode node, int id) {
         SymbolTable varTable = node.getVarSymbolTable();
-        int numFields = varTable.getSize();
+        //// TODO: 3/31/2017 figure this out, if it should be 1 or 2 or 0 or what
+        System.out.println(varTable.getCurrScopeLevel()); // should this be 1?
+        int numFields = varTable.getCurrScopeSize();
         String dispatchTableName = node.getName()+"_dispatch_table";
         assemblySupport.genWord(Integer.toString(id));
-        assemblySupport.genWord(Integer.toString(numFields * 4));//verify that this is the right number
+        assemblySupport.genWord(Integer.toString(numFields * 4));
+        //TODO verify that this is the right number ^^^
         assemblySupport.genWord(dispatchTableName);
         //TODO figure out what the other things in the templates are
         // some of them have words with 0 following them...
     }
 
     /**
-     * Generates the dispatch tables for the given class
+     * Generates the dispatch tables for all the classes in the class tree.
      */
     public void generateDispatchTables(){
+        // order matters here, do parents first
+        Map<Class_, List<String>> methodNameListMap = new HashMap<>();
         for(ClassTreeNode node : this.root.getClassMap().values()){
-            generateClassTemplate(node);
+            generateDispatchTable(node.getASTNode(), methodNameListMap);
         }
     }
 
     /**
-     *
+     * Generates the dispatch table for the given class, and map of pre-existing method
+     * names.
+     * @param clazz    the class to make the table for
+     * @param methodNameListMap     the map of classes to lists of their method names
      */
-    public void generateDispatchTable(){
+    private void generateDispatchTable(Class_ clazz,
+                                       Map<Class_, List<String>> methodNameListMap) {
 
+        //// TODO: 3/30/2017 make sure order of methods is correct, it's very
+        //   important that it matches the example. It should as it's written
+        //   but I haven't tested it yet because I don't know how.
+
+        // if havent done parent yet, do parent first
+        Class_ parent = root.lookupClass(clazz.getParent()).getASTNode();
+        //// TODO: 3/31/2017 figure out if Object has null parent
+        System.out.println("Fix me if necessary, is this null? If so we're good");
+        System.out.println(root.getASTNode().getParent());
+        if (parent != null && !methodNameListMap.containsKey(parent)) {
+            generateDispatchTable(parent, methodNameListMap);
+        }
+
+        List<String> currentTable;
+        if (parent != null) {
+            // copy the dispatch table of the parent
+            currentTable = new ArrayList<>(methodNameListMap.get(parent));
+        } else {
+            // impl: we cannot initialize at declaration because currentTable
+            // must be final or effectively final to be used in the forEach lambda
+            currentTable = new ArrayList<>();
+        }
+
+        // the below code copies the parent dispatch table and overwrites methods if
+        // necessary
+        assemblySupport.genLabel(clazz.getName()+"_dispatch_table");
+        clazz.getMemberList().forEach(n -> {
+            // if its a method then we'll actually do things
+            if (n instanceof Method) {
+                Method m = (Method) n;
+                // checks if we are overriding a method and if so removes it
+                // from the dispatch table.
+                currentTable.stream()
+                        .filter(s -> s.substring(s.indexOf(".")) != m.getName());
+                // adds the method to the symbol table
+                String name = clazz.getName()+"."+m.getName();
+                currentTable.add(name);
+            }
+        });
+        // generate the methods
+        methodNameListMap.get(clazz).forEach(s -> assemblySupport.genWord(s));
     }
 
-
-    /**
-     * This method creates the assmebly for a string constants
-     * @param label the label for the string
-     * @param string the string itself
-     */
-    public void generateStringConstant(String label, String string){
-        int stringLengthRounded = (int) Math.ceil((string.length()+1)/4.0)*4; //remember
-        // to look this line over with dale,
-        //add one or not to add one, that is the question
-        assemblySupport.genLabel(label);
-        assemblySupport.genWord("1"); //says its a string?
-        //Won't this concat?, and isn't that bad?
-
-        // Yes it will lol, leaving the original line
-        // for now because it's hilarious -- Nick
-
-        // assemblySupport.genWord(Integer.toString(16) + stringLengthRounded); //size of
-        // all of this
-        assemblySupport.genWord(Integer.toString(16 + stringLengthRounded)); //size of
-        // all of this
-
-        assemblySupport.genWord("String_dispatch_table"); //pointer to VFT
-        assemblySupport.genWord(Integer.toString(string.length()));//this line is in example, but we should ask dale.
-        assemblySupport.genAscii(string); //the actual string
-        assemblySupport.genByte("0");
-        assemblySupport.genAlign();
-    }
 }
